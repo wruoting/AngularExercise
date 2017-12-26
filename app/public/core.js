67532c875afb44e23938db141af01d30bb0e2321
wruoting <wruoting@gmail.com>
2017-12-26 18:00:34 -0500

    Dependency updates and angular test

--- app/public/core.js
+++ app/public/core.js
@@ -1,2 +1,8 @@
 var scotchTodo = angular.module('scotchTodo', []); //creates a new module called scotchTodo
-  
+console.log("Test")
+function mainController($scope,$http) {
+  $scope.formData = {}
+
+  $http.get('api/todos')
+
+}
